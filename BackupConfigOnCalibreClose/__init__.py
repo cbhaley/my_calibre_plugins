dbb0fa7cd59366861a42957d520af6f08710a79b
Charles Haley <cbhaley@i.wont.say.com>
2025-02-03 17:27:28 +0000

    1) Add a button to choose the folder. 2) Clean up the tooltips

--- BackupConfigOnCalibreClose/__init__.py
+++ BackupConfigOnCalibreClose/__init__.py
@@ -9,10 +9,11 @@ __docformat__ = "restructuredtext en"
 import os
 import shutil
 
-from qt.core import QFormLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QWidget
+from qt.core import QGridLayout, QHBoxLayout, QIcon, QLabel, QLineEdit, QPushButton, QToolButton, QWidget
 
 from calibre import config_dir
 from calibre.customize import LibraryClosedPlugin
+from calibre.gui2 import choose_dir
 from calibre.utils.config import JSONConfig
 from calibre.utils.date import format_date, now
 
@@ -37,31 +38,48 @@ class BackupConfigOnCalibreClose(LibraryClosedPlugin):
     description = 'Backup the calibre configuration folder when calibre is closed'
     author = 'Charles Haley'
     supported_platforms = ['windows', 'osx', 'linux']
-    version = (1, 0, 0)
+    version = (1, 0, 1)
 
     def is_customizable(self):
         return True
 
     def config_widget(self):
         mw = QWidget()
-        l = QFormLayout()
-        l.setFieldGrowthPolicy(QFormLayout.FieldGrowthPolicy.ExpandingFieldsGrow)
+        l = QGridLayout()
         mw.setLayout(l)
-        l.addRow(QLabel('Documentation is in tooltips'))
+
+        w = QLabel('Documentation is in tooltips')
+        w.setContentsMargins(0, 0, 0, 0)
+        l.addWidget(w, 0, 1, 1, 2)
+
         self.config_dir_widget = w = QLineEdit()
         w.setText(plugin_prefs[CONFIG_DIR])
         w.setToolTip('<p>The parent folder where the backup folder will be created. '
-                     'The default is the folder containing the calibre configuration folder</p>')
-        l.addRow('Destination folder:', w)
+                     'The value "{}" is replaced with the path to the folder containing '
+                     'the calibre configuration folder</p>'.format(CONFIG_DIR_PARENT_PATTERN))
+        l.addWidget(QLabel('Containing folder:'), 1, 0)
+        l.addWidget(w, 1, 1)
+        w = self.folder_button = QToolButton()
+        w.setIcon(QIcon.ic('devices/folder.png'))
+        w.setToolTip('Choose a folder')
+        w.clicked.connect(self.choose_folder)
+        l.addWidget(w, 1, 3)
+
         self.name_pattern_widget = w = QLineEdit()
         w.setText(plugin_prefs[NAME_PATTERN])
-        w.setToolTip('<p>The name of the backup folder that will contain the zip backup files. '
-                     'The default is "backup-" followed by the name of the calibre configuration folder</p>')
-        l.addRow('File name pattern:', w)
+        w.setToolTip('<p>The name of the folder that will contain the zip backup files. This folder '
+                     'will be created inside the folder named above (<i>Containing folder</i>). The value '
+                     '"{0}" is replaced with the base name of the configuration folder. The default is '
+                     '"backup-{0}" that becomes "backup-name_of_config_folder"</p>'.format(CONFIG_FOLDER_PATTERN))
+        l.addWidget(QLabel('Backup folder name:'), 2, 0)
+        l.addWidget(w, 2, 1)
+
         self.date_pattern_widget = w = QLineEdit()
         w.setText(plugin_prefs[DATE_PATTERN])
-        w.setToolTip('<p>A standard date pattern. The default is year-month-day at hours-minutes-seconds</p>')
-        l.addRow('Date pattern:', w)
+        w.setToolTip('<p>A standard date pattern used as the name the backup file. '
+                     'The default is year-month-day at hours-minutes-seconds</p>')
+        l.addWidget(QLabel('File name:'), 3, 0)
+        l.addWidget(w, 3, 1)
 
         bl = QHBoxLayout()
         bl.addStretch()
@@ -69,8 +87,18 @@ class BackupConfigOnCalibreClose(LibraryClosedPlugin):
         w.clicked.connect(self.reset_to_defaults)
         bl.addWidget(w)
         bl.addStretch()
-        l.addRow(bl)
+        l.addLayout(bl, 4, 1)
+
+        l.setRowStretch(5, 10)
         return mw
+
+    def choose_folder(self):
+        from calibre.gui2.ui import get_gui
+        gui = get_gui()
+        f = choose_dir(gui, 'backup config on close', 'Select parent folder')
+        if not f:
+            return
+        self.config_dir_widget.setText(f)
 
     def reset_to_defaults(self):
         self.config_dir_widget.setText(CONFIG_DIR_PARENT_PATTERN)
